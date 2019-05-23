26938dcdde6d854fe988d2a7e11aab3b5de62a1a
Vitoria-Cabral <49011309+Vitoria-Cabral@users.noreply.github.com>
2019-05-23 10:01:00 -0300

    java

---
+++ codigojavavitorias.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+	for (int b=50; b<=20; b--){
+	if (b%2==0){
+		printf ("%i Par\n", b);
+	}else{
+		printf ("%i Ímpar\n", b);
+printf (b, "Vamos lá\n");
+return 0;
+}
+}
+}
