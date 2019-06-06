4a68b1f528d0bec007d40db60ae25e29ec3b00e0
vitoria <vitoria.moreira7@gmail.com>
2019-06-06 10:34:50 -0300

    atividade

---
+++ projetovitorias/src/gato/gato.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author aluno
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+//gato.java
+package gatos ;
+public class gato{
+ boolleen femea;
+ String nome;
+ int idade;
+ Gato mae;
+ public Gato(String n){
+     this.nome=n;
+ }
+     public mia(){
+             if(idade>=6){
+         system.ort.print("miar");
+     }else{
+    system.out.print("miau miau");
+}
+}           
+}
+
