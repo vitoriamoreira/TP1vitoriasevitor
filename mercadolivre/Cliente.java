3d03cd66b888003129800c28883c2078565046f0
Vitoria-Cabral <49011309+Vitoria-Cabral@users.noreply.github.com>
2019-07-11 09:55:10 -0300

    aula de java collections- vitória cabral, vitória moreira

--- mercadolivre/Cliente.java
+++ mercadolivre/Cliente.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mercadolivre;
 
-/**
- *
- * @author aluno
- */
-class booleen {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Cliente {
+    String nome;
+    int CPF;
+    int telefone;
+    Cliente endereco;
+    public Cliente(String n){
+        this.nome=n;
+        
+        
+    }
 }
+
+      public class lista {
+            public static void main(String[] args) {    
+            Collection<String> endereco = new ArrayList();
+            endereco.add("caxias");
+            endereco.add("carapebus");
+            endereco.add("rio de janeiro");
+            endereco.add("piteiras");
+            endereco.add("quissamã");
+         
+            System.out.println(“Lista de endereco: ”+endereco);
+    }
+}
+
+  
+
+
+    
+
+
+
