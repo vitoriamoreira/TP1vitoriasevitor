a9dfd0876a922726dd3f9f5ef48350ff3ecfce09
vitoriamoreira <49276118+vitoriamoreira@users.noreply.github.com>
2019-07-11 10:12:52 -0300

    aula java collections- vitória moreira, vitória cabral, lista com conjuntos de endereços separado do codigo cliente.

---
+++ mercadolivre/lista.java
@@ -0,0 +1,22 @@
+
+package mercadolivre;
+import java.util.Collection; 
+import java.util.ArrayList;
+ 
+/**
+ *
+ * @author aluno
+ */
+public class lista {
+    public static void main(String[] args) {    
+        Collection<String>  enderecos;
+        enderecos = new ArrayList();
+        enderecos.add("caxias");
+        enderecos.add("carapebus");
+        enderecos.add("rio de janeiro");
+        enderecos.add("piteiras");
+        enderecos.add("quissamã");
+         
+        System.out.println(“Lista de enderecos: ”+endereco);
+    }
+}
